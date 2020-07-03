bb76e795d70944da8876752e4430dd3e5e384ceb
Paweł Śmigielski <walek.smigielski@gmail.com>
2020-07-03 19:37:51 +0200

    add content component

---
+++ src/components/Content.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="contentWrapper">
+    <h1>Mars rover photos</h1>
+    <p>curiosity: 2012.08.06 to today</p>
+    <p>opportunity: 2004.01.25 to 2018.06.10</p>
+    <p>spirit: 2004.01.04 to 2010.03.22</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+};
+</script>
+
+<style lang="scss" scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400&display=swap');
+
+    .contentWrapper{
+        width: 30%;
+        height: 30%;
+        background-color: rgba(255, 255, 255, 0.3);
+        text-align: center;
+        display: flex;
+        justify-content: center;
+        font-family: 'Noto Sans JP', sans-serif;
+        align-items: center;
+        flex-direction: column;
+        border-radius: 30px;
+    }
+    h1{
+        color: rgba(0, 0, 0, 1);
+        font-weight: 400;
+    }
+    p{
+        color: rgba(0, 0, 0, 1);
+        font-weight: 300;
+    }
+</style>
