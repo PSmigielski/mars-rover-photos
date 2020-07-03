72210fa301885a5db882dd4b2cf3e5917419bebb
Paweł Śmigielski <walek.smigielski@gmail.com>
2020-07-03 13:37:48 +0200

    add background component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="rootWrapper">
     <h1>Mars rover photos</h1>
+    <Background />
   </div>
 </template>
 
 <script>
+import Background from './components/Background.vue';
 
 export default {
   name: 'App',
+  components: {
+    Background,
+  },
 };
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+  .rootWrapper{
+    width: 100vw;
+    height: 100vh;
+  }
+  h1{
+    margin:0px;
+  }
 </style>
---
+++ src/components/Background.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="backgroundWrapper" />
+</template>
+<script>
+export default {
+  name: 'Background',
+};
+</script>
+<style lang="scss" scoped>
+  .backgroundWrapper{
+      background-image: url('../assets/background.jpg');
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      top:0;
+      position: fixed;
+  }
+</style>
